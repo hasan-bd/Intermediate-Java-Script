217c73038e049875c02d9c1cbee27fc9228ab2ea
kamrullive <kamrulhasan570@gmail.com>
2020-04-29 15:02:52 +0600

    Array collection and Guest Names cheking problem solve

---
+++ Array Collection.js
@@ -0,0 +1,19 @@
+// Array Creation and some funnction of array
+
+// var names = ["", "", "", ""]
+// var Myname = names.[1];                  //ei khane index number dore name ber korbe
+// name.length;                    // total koto gula name achhe tai ber hobe
+// nams.includes("")               // check korbe then true or false ta janabe
+
+
+
+
+// Guest list alert useing array
+var guestlist=["kamrul","Shayan","Sadia","Sarina"];
+var guestName = prompt("What is your Name ?");
+
+if (guestlist.includes(guestName)){
+    alert("Welcome")
+}else{
+    alert("Sorry, May be Next Time. ")
+}
