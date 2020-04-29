7f55bc5f80276608b5e8e5f7f5339fb8ea2e77fc
kamrullive <kamrulhasan570@gmail.com>
2020-04-29 14:30:08 +0600

    If Else Advanced love calculator

---
+++ If Else love calculator.js
@@ -0,0 +1,32 @@
+//Advanced Love Calculator...
+
+var nameOne = prompt("What is Your Name? ");
+var nameTwo = prompt("What is Their Name? ");
+var loveScore = Math.floor((Math.random()*100)+1)
+if (loveScore > 70){
+    alert (nameOne+ " And "+nameTwo+ " Your love Score "+loveScore+" %."+ "Your are both best couple");
+
+}else if (loveScore > 30 && loveScore<=70){
+    alert (nameOne+ " And "+nameTwo+ " Your love Score "+loveScore+" %."+ "Your are modarate lover.");
+}else if (loveScore <30) {
+    alert (nameOne+ " And "+nameTwo+ " Your love Score "+loveScore+" %."+ "You to together oil and water.");
+
+}
+
+
+
+//Note Tutorial video comparator and equal sign
+
+
+//         === is equal to
+//         !== is not equalto
+//         > is greater than
+//         < is lesser than
+//         >= is greater or equalto
+//         <= is lesser or equalto
+
+//         Combile operator
+
+//         && And       two conditon is true
+//         || Or        Any one conditon is true
+//         ! Not        No conditon is true.
